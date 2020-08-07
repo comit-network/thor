b8f0730a2f05e6d2e9b70c138d676d75885b107f
Franck Royer <franck@royer.one>
2020-08-07 10:52:26 +1000

    Setup the test to open a channel
    As this is a PoC to showcase the implementation of
    the Generalized Bitcoin-compatible channels, the showcasing
    is highlight by being part of the lib documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+//! # Thor
+//!
+//! A proof of concept of the paper: `Generalized Bitcoin-compatible channels` on top of Bitcoin.
+//!
+//! # Examples
+//!
+//! ## Open a channel
+//!
+//! ```rust
+//! use bitcoin_harness::{Bitcoind, bitcoind_rpc, Client, Wallet};
+//!
+//! # #[tokio::main]
+//! # async fn main() {
+//! // Setting up a Bitcoin regtest environment
+//! let tc_client = testcontainers::clients::Cli::default();
+//! let bitcoind = Bitcoind::new(&tc_client, "0.19.1").unwrap();
+//! let client = Client::new(bitcoind.node_url.clone());
+//! bitcoind.init(5).await.unwrap();
+//!
+//! let alice_wallet = Wallet::new("alice_wallet", bitcoind.node_url.clone()).await.unwrap();
+//! {
+//!     let address = alice_wallet.new_address().await.unwrap();
+//!     let amount = bitcoin::Amount::from_btc(3.0).unwrap();
+//!     bitcoind.mint(address, amount).await.unwrap();
+//! }
+//!
+//! let bob_wallet = Wallet::new("alice_wallet", bitcoind.node_url.clone()).await.unwrap();
+//! {
+//!     let address = bob_wallet.new_address().await.unwrap();
+//!     let amount = bitcoin::Amount::from_btc(3.0).unwrap();
+//!     bitcoind.mint(address, amount).await.unwrap();
+//! }
+//!
+//! # }
+//! ```
+
 #![allow(non_snake_case, unused, unreachable_code)]
 
 pub mod create;
