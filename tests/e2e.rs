d5b50065efa1a2e0b70da254b19da261c01047a5
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2020-08-18 12:50:33 +1000

    Add e2e test for update protocol
    We only verify that both parties end up with the same channel balance
    according to their current `TX_s`. Further tests could verify that
    afterwards both parties can:
    
    - Force close the channel.
    - Publish the revoked TX_c which the other party can punish.
    - Execute another channel update.

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -4,7 +4,11 @@ mod harness;
 
 use bitcoin::Amount;
 use bitcoin_harness::{self, Bitcoind};
-use harness::{make_actors, run_create_protocol, Actors, Created};
+use harness::{
+    make_create_actors, make_update_actors, run_create_protocol, run_update_protocol, CreateActors,
+    Created, UpdateActors, Updated,
+};
+use thor::update::ChannelUpdate;
 
 #[tokio::test]
 async fn e2e_channel_creation() {
@@ -14,12 +18,12 @@ async fn e2e_channel_creation() {
     let time_lock = 1;
     let (alice_balance, bob_balance) = (Amount::ONE_BTC, Amount::ONE_BTC);
 
-    let Actors {
+    let CreateActors {
         alice,
         alice_wallet,
         bob,
         bob_wallet,
-    } = make_actors(&bitcoind, alice_balance, bob_balance, time_lock).await;
+    } = make_create_actors(&bitcoind, alice_balance, bob_balance, time_lock).await;
 
     let Created { alice, bob } = run_create_protocol(&alice_wallet, alice, &bob_wallet, bob).await;
 
@@ -30,4 +34,40 @@ async fn e2e_channel_creation() {
         .send_raw_transaction(alice.signed_TX_f.clone())
         .await
         .unwrap();
+}
+
+#[tokio::test]
+async fn e2e_channel_update() {
+    let tc_client = testcontainers::clients::Cli::default();
+    let bitcoind = Bitcoind::new(&tc_client, "0.19.1").unwrap();
+
+    let time_lock = 1;
+    let (alice_balance, bob_balance) = (Amount::ONE_BTC, Amount::ONE_BTC);
+
+    let CreateActors {
+        alice,
+        alice_wallet,
+        bob,
+        bob_wallet,
+    } = make_create_actors(&bitcoind, alice_balance, bob_balance, time_lock).await;
+
+    let Created { alice, bob } = run_create_protocol(&alice_wallet, alice, &bob_wallet, bob).await;
+
+    assert_eq!(alice.signed_TX_f, bob.signed_TX_f);
+
+    alice_wallet
+        .0
+        .send_raw_transaction(alice.signed_TX_f.clone())
+        .await
+        .unwrap();
+
+    let UpdateActors { alice, bob } = make_update_actors(alice, bob);
+
+    let channel_update = ChannelUpdate::Pay(Amount::from_btc(0.5).unwrap());
+    let time_lock = 1;
+
+    let Updated { alice, bob } = run_update_protocol(alice, bob, channel_update, time_lock);
+
+    assert_eq!(alice.balance().unwrap().ours, bob.balance().unwrap().theirs);
+    assert_eq!(alice.balance().unwrap().theirs, bob.balance().unwrap().ours);
 }
--- tests/harness/mod.rs
+++ tests/harness/mod.rs
@@ -1,21 +1,25 @@
 use bitcoin::{util::psbt::PartiallySignedTransaction, Address, Amount};
 use bitcoin_harness::{bitcoind_rpc::PsbtBase64, Bitcoind};
 use reqwest::Url;
-use thor::create::{Alice0, Bob0, BuildFundingPSBT, Party6, SignFundingPSBT};
+use thor::{
+    create::{self, BuildFundingPSBT, Party6, SignFundingPSBT},
+    update,
+};
+use update::ChannelUpdate;
 
-pub struct Actors {
-    pub alice: Alice0,
+pub struct CreateActors {
+    pub alice: create::Alice0,
     pub alice_wallet: Wallet,
-    pub bob: Bob0,
+    pub bob: create::Bob0,
     pub bob_wallet: Wallet,
 }
 
-pub async fn make_actors(
+pub async fn make_create_actors(
     bitcoind: &Bitcoind<'_>,
     alice_balance: Amount,
     bob_balance: Amount,
     time_lock: u32,
-) -> Actors {
+) -> CreateActors {
     bitcoind.init(5).await.unwrap();
 
     let alice_wallet = Wallet::new("alice", bitcoind.node_url.clone())
@@ -39,10 +43,10 @@ pub async fn make_actors(
         bitcoind.mint(address, bob_balance + buffer).await.unwrap()
     };
 
-    let alice = Alice0::new(alice_balance, time_lock);
-    let bob = Bob0::new(bob_balance, time_lock);
+    let alice = create::Alice0::new(alice_balance, time_lock);
+    let bob = create::Bob0::new(bob_balance, time_lock);
 
-    Actors {
+    CreateActors {
         alice,
         alice_wallet,
         bob,
@@ -57,9 +61,9 @@ pub struct Created {
 
 pub async fn run_create_protocol<W>(
     alice_wallet: &W,
-    alice0: Alice0,
+    alice0: create::Alice0,
     bob_wallet: &W,
-    bob0: Bob0,
+    bob0: create::Bob0,
 ) -> Created
 where
     W: BuildFundingPSBT + SignFundingPSBT,
@@ -103,6 +107,61 @@ where
     Created {
         alice: alice6,
         bob: bob6,
+    }
+}
+
+pub struct UpdateActors {
+    pub alice: update::Party0,
+    pub bob: update::Party0,
+}
+
+pub fn make_update_actors(alice: create::Party6, bob: create::Party6) -> UpdateActors {
+    let alice = update::Party0::new(alice);
+    let bob = update::Party0::new(bob);
+
+    UpdateActors { alice, bob }
+}
+
+pub struct Updated {
+    pub alice: update::Party0,
+    pub bob: update::Party0,
+}
+
+pub fn run_update_protocol(
+    alice0: update::Party0,
+    bob0: update::Party0,
+    channel_update: ChannelUpdate,
+    time_lock: u32,
+) -> Updated {
+    let (alice1, message0) = alice0
+        .propose_channel_update(channel_update, time_lock)
+        .unwrap();
+
+    let (bob1, message1) = bob0.receive_channel_update(message0).unwrap();
+
+    let alice2 = alice1.receive(message1).unwrap();
+
+    let message2_alice = alice2.next_message();
+    let message2_bob = bob1.next_message();
+
+    let alice3 = alice2.receive(message2_bob).unwrap();
+    let bob2 = bob1.receive(message2_alice).unwrap();
+
+    let message3_alice = alice3.next_message();
+    let message3_bob = bob2.next_message();
+
+    let alice4 = alice3.receive(message3_bob).unwrap();
+    let bob3 = bob2.receive(message3_alice).unwrap();
+
+    let message4_alice = alice4.next_message();
+    let message4_bob = bob3.next_message();
+
+    let alice5 = alice4.receive(message4_bob).unwrap();
+    let bob4 = bob3.receive(message4_alice).unwrap();
+
+    Updated {
+        alice: alice5,
+        bob: bob4,
     }
 }
 
