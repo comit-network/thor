a77a8a9152563221b0a0d69f5a16abacab831dbe
Tobin C. Harding <tobin@coblox.tech>
2020-09-09 14:32:29 +1000

    Remove generate_balances helper
    This helper is unnecessary, the code is brain dead simple just inline it.

--- thor/tests/harness/mod.rs
+++ thor/tests/harness/mod.rs
@@ -16,18 +16,14 @@ mod wallet;
 pub use wallet::{make_wallets, Wallet};
 
 pub fn generate_balances(fund_amount: Amount) -> (Balance, Balance) {
-    _generate_balances(fund_amount, fund_amount)
-}
-
-fn _generate_balances(a_fund_amount: Amount, b_fund_amount: Amount) -> (Balance, Balance) {
     let a_balance = Balance {
-        ours: a_fund_amount,
-        theirs: b_fund_amount,
+        ours: fund_amount,
+        theirs: fund_amount,
     };
 
     let b_balance = Balance {
-        ours: b_fund_amount,
-        theirs: a_fund_amount,
+        ours: fund_amount,
+        theirs: fund_amount,
     };
 
     (a_balance, b_balance)
